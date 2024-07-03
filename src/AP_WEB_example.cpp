179741135cf04922f871f7585d60a24151fb01ce
Sergio Carmo <19749@ipportalegre.pt>
2024-07-03 05:50:27 +0100

    03-07-layout

--- src/AP_WEB_example.cpp
+++ src/AP_WEB_example.cpp
@@ -3,7 +3,7 @@
 #include "AP.h"			// Definitions
 #include "AP_WebPage_example.h" // WebPage HTML content
 
-WebServer server(80);
+WebServer server(80); // Create a webserver object that listens for HTTP request on port 80
 
 //===============================================================
 // This routine is executed when you open its IP in browser
@@ -15,7 +15,7 @@ void handleRoot() {
 //===============================================================
 // Timer handlers
 //===============================================================
-void handleTimer() {
+void handleTimer() {  
   String timer = String(getTimerHour()) + "h :" + String(getTimerMinute()) + "m :" + String(getTimerSecound())+ "s";
   server.send(200, "text/plain", timer); //Send ADC value only to client ajax request
 }
@@ -96,6 +96,8 @@ void handlerem10m() {
 }
 
 //===============================================================
+// Timer Start/Stop/Reset
+//===============================================================
 void handleTimerStart() {
   setTimer(ON);
   String timerbuttons = " ";
@@ -120,26 +122,75 @@ void handleAuto() {
   server.send(200, "text/plain", autoMode); //Send ADC value only to client ajax request
 }
 
+void handlePump() {
+  String pump = String(getPump());
+  server.send(200, "text/plain", pump); //Send ADC value only to client ajax request
+}
+
+void handleDumpWater() {
+  String dumpWater = String(getValv_Water_Out());
+  server.send(200, "text/plain", dumpWater); //Send ADC value only to client ajax request
+}
+
 void handleResistor() {
   String resistor = String(getResistor());
   server.send(200, "text/plain", resistor); //Send ADC value only to client ajax request
 }
 
+void handleWaterIn() { // Call handleWaterIn() function
+  String waterIn = String(getValv_Water_In());
+  server.send(200, "text/plain", waterIn); //Send ADC value only to client ajax request
+}
+
+void handleWaterMax() { // Call handleWaterIn() function
+  String waterMax = String(getWaterMax());
+  server.send(200, "text/plain", waterMax); //Send ADC value only to client ajax request
+}
+
+void handleWaterMin() { // Call handleWaterIn() function
+  String waterMin = String(getWaterMin());
+  server.send(200, "text/plain", waterMin); //Send ADC value only to client ajax request
+}
+
+void handleWaterAlarm() { // Call handleWaterIn() function
+  String waterAlarm = String(getWaterAlarm());
+  server.send(200, "text/plain", waterAlarm); //Send ADC value only to client ajax request
+}
+
 void handleToggleAuto() {
   toggleAutoModeWEB(); // Call the toggleAutoMode() function
-  Serial.println("handleToggleAuto");
   server.send(200, "text/plain", "Auto mode toggled"); // Send response to client
 }
 
+void handleTogglePump() {
+  togglePump(); // Call the toggleAutoMode() function
+  server.send(200, "text/plain", "Pump toggled"); // Send response to client
+}
+
+void handleToggleDumpWater() {
+  toggleValveWaterOut(); // Call the toggleAutoMode() function
+  server.send(200, "text/plain", "Dump Condensor toggled"); // Send response to client
+}
+
+void handleToggleWaterIn() {
+  toggleValveWaterIn(); // Call the toggleAutoMode() function
+  server.send(200, "text/plain", "Water In toggled"); // Send response to client
+}
+
+void handleToggleResistor() {
+  sPrintLnStr("toggleResistor");
+  toggleResistor(); // Call the toggleAutoMode() function
+  server.send(200, "text/plain", "Resistor toggled"); // Send response to client
+}
 
 //===============================================================
 // Setup Calls
 //===============================================================
 
 void setupCalls() {
-  server.on("/", handleRoot);      // This is display page
+  server.on("/", handleRoot);  //Which routine to handle at root location. This is display page
   ///////
-  server.on("/readTimer", handleTimer);
+  server.on("/readTimer", handleTimer); // Call handleTimer() function
   ///////
   server.on("/add1s", handleAdd1s);
   server.on("/add5s", handleAdd5s);
@@ -153,23 +204,33 @@ void setupCalls() {
   server.on("/rem10s", handlerem10s);
   server.on("/rem1m",  handlerem1m);
   server.on("/rem5m",  handlerem5m);
-  server.on("/rem10m", handlerem10m);
+  server.on("/rem10m", handlerem10m); 
   ///////
-  server.on("/timerStart", handleTimerStart);
-  server.on("/timerStop", handleTimerStop);
-  server.on("/timerReset", handleTimerReset);
+  server.on("/timerStart", handleTimerStart); // Call handleTimerStart() function
+  server.on("/timerStop", handleTimerStop); // Call handleTimerStop() function
+  server.on("/timerReset", handleTimerReset); // Call handleTimerReset() function
 
-  server.on("/readAuto", handleAuto);
-  server.on("/readResistor", handleResistor); // Call handleResistor() function
-  server.on("/toggleAuto", handleToggleAuto); // Call toggleAutoMode() function
-  server.begin();                  // Start server
+  server.on("/readAuto", handleAuto);                     // Call handleAuto() function
+  server.on("/toggleAuto", handleToggleAuto);             // Call handleToggleAuto() function
+  server.on("/readPump", handlePump);                     // Call handlePump() function
+  server.on("/togglePump", handleTogglePump);                   // Call handlePump() function
+  server.on("/readDumpWater", handleDumpWater);   // Call handleDumpCondensor() function
+  server.on("/toggleDumpWater", handleToggleDumpWater); // Call handleDumpCondensor() function
+  server.on("/readWaterIn", handleWaterIn);               // Call handleWaterIn() function
+  server.on("/toggleWaterIn", handleToggleWaterIn);             // Call handleWaterIn() function
+  server.on("/readResistor", handleResistor);             // Call handleResistor() function
+  server.on("/toggleResistor", handleToggleResistor);           // Call handleResistor() function
+  server.on("/readWaterMax", handleWaterMax);   // Call handleDumpCondensor() function
+  server.on("/readWaterMin", handleWaterMin);   // Call handleDumpCondensor() function
+  server.on("/readWaterAlarm", handleWaterAlarm);   // Call handleDumpCondensor() function
+  server.begin();                                         // Start the HTTP server
   Serial.println("HTTP server started");
 }
 
 //===============================================================
 // This routine is executed when you open its IP in browser
 //===============================================================
-void ProcessWebPage() {
-  server.handleClient();
-  delay(1);
+void ProcessWebPage() { 
+  server.handleClient(); //Handle client requests
+  delay(1); // Allow the web server to process the request
 }
--- src/AP_WebPage_example.h
+++ src/AP_WebPage_example.h
@@ -1,148 +1,187 @@
 const char MAIN_page[] PROGMEM = R"=====(
-  <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
-  <style>
-    .card {
-      max-width: 500px;
-      min-height: 850px;
-      background: #02b875;
-      padding: 5px;
-      color: #FFF;
-      margin: 0 auto;
-      box-shadow: 0px 20px 18px -4px rgba(255, 160, 0, 1);
-    }
+  <head>
+    <title>Intelligent Distiller</title>
+    <link rel="icon" href="test-tube.png">
+    <style>
+      .card {
+        max-width: 500px;
+        min-height: 850px;
+        background: #013220;
+        padding: 5px;
+        color: #FFF;
+        margin: 0 auto;
+        box-shadow: 0px 0px 18px -4px rgba(255, 160, 0, 1);
+      }
 
-    html {
-      font-family: Helvetica;
-      display: inline-block;
-      margin: 0px auto;
-      text-align: center;
-    }
+      html {
+        font-family: Helvetica;
+        display: inline-block;
+        margin: 0px auto;
+        text-align: center;
+      }
 
-    .button {
-      background-color: #4C7AAE;
-      border: none;
-      border-radius: 15px;
-      color: white;
-      padding: 10px 10px;
-      text-decoration: none;
-      font-size: 20px;
-      margin: 2px;
-      cursor: pointer;
-    }
+      .button {
+        transition-duration: 0.4s;
+        background-color: #4C7AAE;
+        border: none;
+        border-radius: 15px;
+        color: white;
+        padding: 10px 10px;
+        text-decoration: none;
+        font-size: 20px;
+        margin: 2px;
+        cursor: pointer;
+      }
 
-    button:hover {
-      background-color: blue;
-    }
+      button.button:hover {
+        background-color: blue;
+      }
 
-    button:active {
-      background-color: red;
-    }
+      button.button:active {
+        background-color: red;
+      }
 
-    .button2 {
-      background-color: #555555;
-      border: none;
-      border-radius: 15px;
-      color: white;
-      padding: 10px 10px;
-      text-decoration: none;
-      font-size: 20px;
-      margin: 2px;
-      cursor: pointer;
-    }
+      .buttonWater {
+        background-color: #000000;
+        border: none;
+        border-radius: 15px;
+        color: white;
+        padding: 10px 10px;
+        text-decoration: none;
+        font-size: 20px;
+        margin: 2px;
+        cursor: pointer;
+        font: bold 18pt Arial, Helvetica, sans-serif;
+      }
 
-    body {
-      background-color: #191970;
-      font-size: 100%;
-      color: #FFFFFF;
-    }
+      body {
+        background-color: #191970;
+        font-size: 100%;
+        color: #FFFFFF;
+        <title>Intelligent Distiller</title><link rel="icon"href="img/test-tube.png">
+      }
 
-    #main {
-      display: table;
-      margin: auto;
-      padding: 30px 30px 30px 30px;
-    }
+      #main {
+        display: table;
+        margin: auto;
+        padding: 30px 30px 30px 30px;
+      }
 
-    #content {
-      border: 2px solid blue;
-      border-radius: 15px;
-      padding: 10px 10px 10px 10px;
-    }
+      #content {
+        border: 2px solid blue;
+        border-radius: 15px;
+        padding: 10px 10px 10px 10px;
+      }
 
-    h2 {
-      text-align: center;
-      margin: 10px 0px 10px 0px;
-    }
+      h2 {
+        text-align: center;
+        margin: 10px 0px 10px 0px;
+      }
 
-    p {
-      text-align: center;
-      margin: 5px 0px 10px 0px;
-      font-size: 120%;
-    }
+      p {
+        text-align: center;
+        margin: 5px 0px 10px 0px;
+        font-size: 120%;
+      }
 
-    #time_P {
-      margin: 10px 0px 15px 0px;
-    }
+      #time_P {
+        margin: 10px 0px 15px 0px;
+      }
 
-    blc {
-      display: inline-block;
-    }
-
-    @media,
-      {
       blc {
-        display: inline;
-        margin-right: 10px;
-        text-align: center;
+        display: inline-block;
       }
 
-      blc2 {
-        display: inline;
-        margin-right: 10px;
-        line-height: 10px;
-        text-align: center;
-      }
+      @media,
+        {
+        blc {
+          display: inline;
+          margin-right: 10px;
+          text-align: center;
+        }
 
-      blc3 {
-        display: inline;
-        margin-left: 10px;
-        line-height: 10px;
-        text-align: center;
+        blc2 {
+          display: inline;
+          margin-right: 10px;
+          line-height: 10px;
+          text-align: center;
+        }
+
+        blc3 {
+          display: inline;
+          margin-left: 10px;
+          line-height: 10px;
+          text-align: center;
+        }
       }
-    }
-  </style>
+    </style>
+  </head>
   <body>
+    <div id='main'>
+      <h2>Intelligent Distiller</h2>
+      <hr>
+    </div>
     <div class="card">
       <h3>
         <span id="timerbuttons"></span>
       </h3>
       <br>
-      <button class="button" onclick="startTimer()">Start</button>
-      <button class="button" onclick="stopTimer()">Stop</button>
-      <button class="button" onclick="resetTimer()">Reset</button>
+      <button id="ButtonStartTimer" class="button" onclick="startTimer()">Start</button>
+      <button id="ButtonStopTimer" class="button" onclick="stopTimer()">Stop</button>
+      <button id="ButtonResetTimer" class="button" onclick="resetTimer()">Reset</button>
       <br>
       <h3>Timer: <span id="timer">0</span>
       </h3>
       <br>
-      <button class="button" onclick="add1s()">+1s</button>
-      <button class="button" onclick="add5s()">+5s</button>
-      <button class="button" onclick="add10s()">+10s</button>
-      <button class="button" onclick="add1m()">+1m</button>
-      <button class="button" onclick="add5m()">+5m</button>
-      <button class="button" onclick="add10m()">+10m</button>
+      <button id="ButtonAdd1s" class="button" onclick="add1s()">+1s</button>
+      <button id="ButtonAdd5s" class="button" onclick="add5s()">+5s</button>
+      <button id="ButtonAdd10s" class="button" onclick="add10s()">+10s</button>
+      <button id="ButtonAdd1m" class="button" onclick="add1m()">+1m</button>
+      <button id="ButtonAdd5m" class="button" onclick="add5m()">+5m</button>
+      <button id="ButtonAdd10m" class="button" onclick="add10m()">+10m</button>
       <br>
-      <button class="button" onclick="rem1s()">-1s</button>
-      <button class="button" onclick="rem5s()">-5s</button>
-      <button class="button" onclick="rem10s()">-10s</button>
-      <button class="button" onclick="rem1m()">-1m</button>
-      <button class="button" onclick="rem5m()">-5m</button>
-      <button class="button" onclick="rem10m()">-10m</button>
+      <button id="ButtonRem1s" class="button" onclick="rem1s()">-1s</button>
+      <button id="ButtonRem5s" class="button" onclick="rem5s()">-5s</button>
+      <button id="ButtonRem10s" class="button" onclick="rem10s()">-10s</button>
+      <button id="ButtonRem1m" class="button" onclick="rem1m()">-1m</button>
+      <button id="ButtonRem5m" class="button" onclick="rem5m()">-5m</button>
+      <button id="ButtonRem10m" class="button" onclick="rem10m()">-10m</button>
+      <br>
+      <hr>
+      <br>
+      <button id="ButtonToggleAuto" class="button" onclick="toggleAutoMode()">Auto</button>
+      <button id="ButtonTogglePump" class="button" onclick="togglePump()">Pump</button>
+      <button id="ButtonToggleWaterIn" class="button" onclick="toggleWaterIn()">Water In</button>
+      <br>
+      <button id="ButtonToggleResistor" class="button" onclick="toggleResistor()">Resistor</button>
+      <button id="ButtonToggleDumpWater" class="button" onclick="toggleDumpWater()">Dump Water</button>
+      <br>
+      <hr>
+      <br>
+      <button id="ButtonWaterMax" class="buttonWater">Water Max</button>
+      <button id="ButtonWaterMin" class="buttonWater">Water Min</button>
+      <button id="ButtonWaterAlarm" class="buttonWater">Water Alarm</button>
+      <br>
+      <hr>
       <br>
       <h3>Auto mode: <span id="autoMode">0</span>
       </h3>
       <h3>Resistor: <span id="resistor">0</span>
       </h3>
-      <button class="button" onclick="toggleAutoMode()">Auto</button>
+      <h3>Pump: <span id="pump">0</span>
+      </h3>
+      <h3>Water In: <span id="waterIn">0</span>
+      </h3>
+      <h3>Dump Water: <span id="dumpWater">0</span>
+      </h3>
+      <h3>Water Min: <span id="waterMin">0</span>
+      </h3>
+      <h3>Water Max: <span id="waterMax">0</span>
+      </h3>
+      <h3>Water Alarm: <span id="waterAlarm">0</span>
+      </h3>
     </div>
     <script>
       setInterval(function() {
@@ -150,6 +189,52 @@ const char MAIN_page[] PROGMEM = R"=====(
         getTimerData();
         getAutoData();
         getResistorData();
+        getDumpWaterData();
+        getWaterInData();
+        getPumpData();
+        getWaterMinData();
+        getWaterMaxData();
+        getWaterAlarmData();
+        if (document.getElementById("autoMode").innerHTML == "1") {
+          document.getElementById("ButtonToggleAuto").style.border = "4px solid #00ff00";
+        } else {
+          document.getElementById("ButtonToggleAuto").style.border = "4px solid #000000";
+        }
+        if (document.getElementById("resistor").innerHTML == "1") {
+          document.getElementById("ButtonToggleResistor").style.border = "4px solid #00ff00";
+        } else {
+          document.getElementById("ButtonToggleResistor").style.border = "4px solid #000000";
+        }
+        if (document.getElementById("pump").innerHTML == "1") {
+          document.getElementById("ButtonTogglePump").style.border = "4px solid #00ff00";
+        } else {
+          document.getElementById("ButtonTogglePump").style.border = "4px solid #000000";
+        }
+        if (document.getElementById("waterIn").innerHTML == "1") {
+          document.getElementById("ButtonToggleWaterIn").style.border = "4px solid #00ff00";
+        } else {
+          document.getElementById("ButtonToggleWaterIn").style.border = "4px solid #000000";
+        }
+        if (document.getElementById("dumpWater").innerHTML == "1") {
+          document.getElementById("ButtonToggleDumpWater").style.border = "4px solid #00ff00";
+        } else {
+          document.getElementById("ButtonToggleDumpWater").style.border = "4px solid #000000";
+        }
+        if (document.getElementById("waterMax").innerHTML == "1") {
+          document.getElementById("ButtonWaterMax").style.backgroundColor = "green";
+        } else {
+          document.getElementById("ButtonWaterMax").style.backgroundColor = "grey";
+        }
+        if (document.getElementById("waterMin").innerHTML == "1") {
+          document.getElementById("ButtonWaterMin").style.backgroundColor = "orange";
+        } else {
+          document.getElementById("ButtonWaterMin").style.backgroundColor = "grey";
+        }
+        if (document.getElementById("waterAlarm").innerHTML == "1") {
+          document.getElementById("ButtonWaterAlarm").style.backgroundColor = "red";
+        } else {
+          document.getElementById("ButtonWaterAlarm").style.backgroundColor = "grey";
+        }
       }, 1000); //1000 mSeconds update rate
       function getTimerData() {
         var xhttp = new XMLHttpRequest();
@@ -327,6 +412,28 @@ const char MAIN_page[] PROGMEM = R"=====(
         xhttp.send();
       }
 
+      function toggleDumpWater() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("toggleDumpWater").innerHTML = this.responseText;
+          }
+        };
+        xhttp.open("GET", "toggleDumpWater", true);
+        xhttp.send();
+      }
+
+      function toggleResistor() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("toggleResistor").innerHTML = this.responseText;
+          }
+        };
+        xhttp.open("GET", "toggleResistor", true);
+        xhttp.send();
+      }
+
       function toggleAutoMode() {
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
@@ -335,6 +442,28 @@ const char MAIN_page[] PROGMEM = R"=====(
           }
         };
         xhttp.open("GET", "toggleAuto", true);
+        xhttp.send();
+      }
+
+      function togglePump() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("togglePump").innerHTML = this.responseText;
+          }
+        };
+        xhttp.open("GET", "togglePump", true);
+        xhttp.send();
+      }
+
+      function toggleWaterIn() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("toggleWaterIn").innerHTML = this.responseText;
+          }
+        };
+        xhttp.open("GET", "toggleWaterIn", true);
         xhttp.send();
       }
 
@@ -359,8 +488,72 @@ const char MAIN_page[] PROGMEM = R"=====(
         xhttp.open("GET", "readResistor", true);
         xhttp.send();
       }
+
+      function getDumpWaterData() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("dumpWater").innerHTML = this.responseText;
+          }
+        };
+        xhttp.open("GET", "readDumpWater", true);
+        xhttp.send();
+      }
+
+      function getWaterInData() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("waterIn").innerHTML = this.responseText;
+          }
+        };
+        xhttp.open("GET", "readWaterIn", true);
+        xhttp.send();
+      }
+
+      function getPumpData() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("pump").innerHTML = this.responseText;
+          }
+        };
+        xhttp.open("GET", "readPump", true);
+        xhttp.send();
+      }
+
+      function getWaterMinData() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("waterMin").innerHTML = this.responseText;
+          }
+        };
+        xhttp.open("GET", "readWaterMin", true);
+        xhttp.send();
+      }
+
+      function getWaterMaxData() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("waterMax").innerHTML = this.responseText;
+          }
+        };
+        xhttp.open("GET", "readWaterMax", true);
+        xhttp.send();
+      }
+
+      function getWaterAlarmData() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("waterAlarm").innerHTML = this.responseText;
+          }
+        };
+        xhttp.open("GET", "readWaterAlarm", true);
+        xhttp.send();
+      }
     </script>
   </body>
-</html>
-)=====";
-
+</html> )=====";
