5d66494e666bd9f440a8c35a85ee0c53d6144093
Sergio Carmo <19749@ipportalegre.pt>
2024-07-19 18:21:01 +0100

    chore: Update CSS files and optimize code

--- data/script_timers.js
+++ data/script_timers.js
@@ -51,48 +51,63 @@ setInterval(function () {
 }, 1389);
 
 /*************************************************************************/
-/* Timer to update water levels indicators                                *
+/* Timer for EN/Disable buttons                                           *
 /* ***********************************************************************/
 setInterval(function () {
-    if (document.getElementById("pump").innerHTML == "1") {
-        document.getElementById("BMB_img").src = "BMB_ON.png";
+    if (document.getElementById("secound").innerHTML == "0" && document.getElementById("minute").innerHTML == "0" && document.getElementById("hour").innerHTML == "0") {
+        document.getElementById("ButtonStartTimer").classList.remove("button");
+        document.getElementById("ButtonStartTimer").classList.add("buttonDisabled");
+        document.getElementById("ButtonStopTimer").classList.remove("button");
+        document.getElementById("ButtonStopTimer").classList.add("buttonDisabled");
+        document.getElementById("ButtonResetTimer").classList.remove("button");
+        document.getElementById("ButtonResetTimer").classList.add("buttonDisabled");
+
+        document.getElementById("ButtonRem1s").classList.add("buttonDisabled");
+        document.getElementById("ButtonRem5s").classList.add("buttonDisabled");
+        document.getElementById("ButtonRem10s").classList.add("buttonDisabled");
+        document.getElementById("ButtonRem1m").classList.add("buttonDisabled");
+        document.getElementById("ButtonRem5m").classList.add("buttonDisabled");
+        document.getElementById("ButtonRem10m").classList.add("buttonDisabled");
+
+        document.getElementById("ButtonRem1s").classList.remove("button");
+        document.getElementById("ButtonRem5s").classList.remove("button");
+        document.getElementById("ButtonRem10s").classList.remove("button");
+        document.getElementById("ButtonRem1m").classList.remove("button");
+        document.getElementById("ButtonRem5m").classList.remove("button");
+        document.getElementById("ButtonRem10m").classList.remove("button");
+
     } else {
-        document.getElementById("BMB_img").src = "BMB_OFF.png";
+        document.getElementById("ButtonStartTimer").classList.add("button");
+        document.getElementById("ButtonStartTimer").classList.remove("buttonDisabled");
+        document.getElementById("ButtonStopTimer").classList.add("button");
+        document.getElementById("ButtonStopTimer").classList.remove("buttonDisabled");
+        document.getElementById("ButtonResetTimer").classList.add("button");
+        document.getElementById("ButtonResetTimer").classList.remove("buttonDisabled");
+
+        document.getElementById("ButtonRem1s").classList.remove("buttonDisabled");
+        document.getElementById("ButtonRem5s").classList.remove("buttonDisabled");
+        document.getElementById("ButtonRem10s").classList.remove("buttonDisabled");
+        document.getElementById("ButtonRem1m").classList.remove("buttonDisabled");
+        document.getElementById("ButtonRem5m").classList.remove("buttonDisabled");
+        document.getElementById("ButtonRem10m").classList.remove("buttonDisabled");
+
+        document.getElementById("ButtonRem1s").classList.add("button");
+        document.getElementById("ButtonRem5s").classList.add("button");
+        document.getElementById("ButtonRem10s").classList.add("button");
+        document.getElementById("ButtonRem1m").classList.add("button");
+        document.getElementById("ButtonRem5m").classList.add("button");
+        document.getElementById("ButtonRem10m").classList.add("button");
     }
 
-    if (document.getElementById("waterIn").innerHTML == "1") {
-        document.getElementById("valve1_img").classList.remove("valve1_off");
-        document.getElementById("valve1_img").classList.add("valve1_on");
-        document.getElementById("valve1_img").src = "Valve_ON.png";
+    if (document.getElementById("autoMode").innerHTML == "0") {
+        document.getElementById("ButtonStartTimer").classList.remove("button");
+        document.getElementById("ButtonStartTimer").classList.add("buttonDisabled");
+        document.getElementById("ButtonStopTimer").classList.remove("button");
+        document.getElementById("ButtonStopTimer").classList.add("buttonDisabled");
     } else {
-        document.getElementById("valve1_img").classList.remove("valve1_on");
-        document.getElementById("valve1_img").classList.add("valve1_off");
-        document.getElementById("valve1_img").src = "Valve_OFF.png";
+        document.getElementById("ButtonStartTimer").classList.add("button");
+        document.getElementById("ButtonStartTimer").classList.remove("buttonDisabled");
+        document.getElementById("ButtonStopTimer").classList.add("button");
+        document.getElementById("ButtonStopTimer").classList.remove("buttonDisabled");
     }
-
-    if (document.getElementById("dumpWater").innerHTML == "1") {
-        document.getElementById("valve2_img").classList.remove("valve2_off");
-        document.getElementById("valve2_img").classList.add("valve2_on");
-        document.getElementById("valve2_img").src = "Valve_ON.png";
-    } else {
-        document.getElementById("valve2_img").classList.remove("valve2_on");
-        document.getElementById("valve2_img").classList.add("valve2_off");
-        document.getElementById("valve2_img").src = "Valve_OFF.png";
-    }
-
-    if (document.getElementById("resistor").innerHTML == "1") {
-        document.getElementById("RAQ_img").src = "RAQ_ON.png";
-    } else {
-        document.getElementById("RAQ_img").src = "RAQ_OFF.png";
-    }
-
-    if (document.getElementById("waterMax").innerHTML == "1") {
-        document.getElementById("tank").src = "sMax.png";
-    }
-    if (document.getElementById("waterMin").innerHTML == "1") {
-        document.getElementById("tank").src = "sMin.png";
-    }
-    if (document.getElementById("waterAlarm").innerHTML == "1" && document.getElementById("waterMin").innerHTML == "1") {
-        document.getElementById("tank").src = "sAlarm.png";
-    }
-}, 1423);
+}, 1429);
